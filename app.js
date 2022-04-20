37bba2513a13095fdb2ceb340d08c081b071eda6
Andrea Oboe <andrea.oboe@allianz.it>
2022-04-20 15:03:20 +0200

    Complete Doubly Linked List

--- app.js
+++ app.js
@@ -1,30 +1,49 @@
 const SinglyLinkedList = require('./src/SinglyLinkedList')
 const DoublyLinkedList = require('./src/DoublyLinkedList')
 
-// console.log(' --- SINGLY LINKED LIST ---')
-// console.log('Create empty singly linked list and add (in this order) at the head 10 and 20...')
-// const ll = new SinglyLinkedList()
-// ll.insertAtHead(10)
-// ll.insertAtHead(20)
-// ll.print()
+//#region Singly Linked List
+console.log(' --- SINGLY LINKED LIST ---')
+console.log('Create empty singly linked list and add (in this order) at the head 10 and 20...')
+const ll = new SinglyLinkedList()
+ll.insertAtHead(10)
+ll.insertAtHead(20)
+ll.print()
 
-// console.log('Create new Singly Linked List from 10 to 50...')
-// const la = SinglyLinkedList.fromValues(10, 20, 30, 40, 50)
-// la.print()
+console.log('Create new Singly Linked List from 10 to 50...')
+const la = SinglyLinkedList.fromValues(10, 20, 30, 40, 50)
+la.print()
 
-// console.log('Insert at index 2 the value 60...')
-// la.insertAtIndex(2, 60)
-// la.print()
+console.log('Insert at index 2 the value 60...')
+la.insertAtIndex(2, 60)
+la.print()
 
-// console.log('Removed the head...')
-// la.removeHead()
-// la.print()
+console.log('Remove the head...')
+la.removeHead()
+la.print()
 
-// console.log('Remove at index 3...')
-// la.removeAtIndex(3)
-// la.print()
-// console.log('---------------------------------------')
+console.log('Remove at index 3...')
+la.removeAtIndex(3)
+la.print()
+
+console.log('---------------------------------------')
+//#endregion
+
+//#region Doubly Linked List
 console.log(' --- DOUBLY LINKED LIST ---')
-console.log('Create new Doubly Linked List from 10 to 40')
+console.log('Create new Doubly Linked List from 10 to 40...')
 const dll = DoublyLinkedList.fromValues(10, 20, 30, 40)
 dll.print()
+
+console.log('Remove the last node from the list...')
+dll.pop()
+dll.print()
+
+console.log('Add new head...')
+dll.unshift(50)
+dll.print()
+
+console.log('Remove the head...')
+dll.shift()
+dll.print()
+
+//#endregion
--- src/DoublyLinkedList.js
+++ src/DoublyLinkedList.js
@@ -5,6 +5,22 @@ class DoublyLinkedList {
         this.length = 0
     }
 
+    getByIndex(index) {
+        if (index < 0 || index >= this.length) return null
+
+        let current = this.head
+        for (let i = 0; i < index; i++) {
+            current = current.next
+        }
+
+        return current
+    }
+
+    /**
+     * The push() mehod add new node to the end of the Doubly Linked List
+     * @param {*} value of the new node
+     * @returns the new Doubly Linked List with the new last node
+     */
     push(value) {
         let node = new DoublyLinkedListNode(value)
         if (!this.head) {
@@ -21,6 +37,69 @@ class DoublyLinkedList {
         return this
     }
 
+    /**
+     * The pop() method removes the last element from the Doubly Linked List
+     * @returns the new Doubly Linked List without the last node
+     */
+    pop() {
+        if (!this.head) return undefined
+        let temp = this.tail
+        if (this.length === 1) {
+            this.head = null
+            this.tail = null
+        }
+        else {
+            this.tail = temp.previous
+            this.tail.next = null
+            temp.previous = null
+        }
+        this.length--
+        return this
+    }
+
+    /**
+     * The shift() method removes the first element from the Doubly Linked List
+     * @returns the new Doubly Linked List without the first node
+     */
+    shift() {
+        if (!this.head) return undefined
+        let temp = this.head
+        if (this.length === 1) {
+            this.head = null
+            this.tail = null
+        }
+        else {
+            this.head = temp.next
+            this.head.previous = null
+            temp.next = null
+        }
+        this.length--
+        return this
+    }
+
+    /**
+     * The unshift() method adds the new node to the head of the current Doubly Linked List
+     * @param {*} val Value of the new node
+     * @returns the new Doubly Linked List with the new head node
+     */
+    unshift(val) {
+        let node = new DoublyLinkedListNode(val)
+        if (!this.head) {
+            this.head = node
+            this.tail = node
+        } else {
+            let temp = this.head
+            this.head = node
+            node.next = temp
+            temp.previous = node
+        }
+        this.length++
+        return this
+    }
+
+    /**
+     * Print the Doubly Linked List in human readble mode
+     */
     print() {
         let output = 'null'
 
